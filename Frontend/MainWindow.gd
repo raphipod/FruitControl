b4f3e21b283f86a4e01bb69af763a627ce6d897e
Raphipod <podraphi@googlemail.com>
2021-10-27 19:34:53 +0200

    added Search button & structural changes

--- Frontend/MainWindow.gd
+++ Frontend/MainWindow.gd
@@ -1,22 +1,27 @@
-############################################
-# File: MainWindow.gd					   #
-#										   #
-# This file combines backend and frontend. #
-############################################
-
 extends Control
 
 var device_name_output = []
 var device_cpuarch_output = []
 var device_osver_output = []
 
+var device_available_output = []
+
 var device_name = OS.execute("idevicename", [], true, device_name_output)
 var device_cpuarch = OS.execute("ideviceinfo", ["|", "awk", "'{print $2}'"], true, device_cpuarch_output)
 # var device_osver = OS.execute("ideviceinfo", ["|", "awk", "'{print $2}'"], true, device_osver_output)
 
+func _on_Search_Button_pressed():
+	
+	var device_available = OS.execute("idevicename", [], true, device_available_output)
+	
+	if device_available_output == null && "[]":
+		print("[DEBUG] iCC: No device found or libimobiledevice not available!")
+	elif device_available_output == '[*]':
+		print("[DEBUG] iCC: Device found!")
+
 func _ready():
 	# Debug output
-	print("+++ BACKENDDBG +++")
+	print("\n+++ [DEBUG] - iCC +++\n")
 	print("Device name: " , device_name_output)
 	print("Device CPU Arch: " , device_cpuarch_output)
 	# print("Device OS Ver: " , device_osver_output)
