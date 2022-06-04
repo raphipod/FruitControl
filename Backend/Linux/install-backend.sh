d795ea0f06968738f7d1092ce6d4b24b38f6cd93
Raphipod <56308821+raphipod@users.noreply.github.com>
2022-06-04 23:22:18 +0000

    Added faster dev setup shell script

--- Backend/Linux/install-backend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://launchpad.net/~quamotion/+archive/ubuntu/ppa/+files/libimobiledevice-utils_1.3.1-1.3.17-0focal_amd64.deb
-sudo dpkg -i libimobiledevice-dev_1.3.1-1.3.17-0focal_amd64.deb
-sudo rm -rf libimobiledevice-dev_1.3.1-1.3.17-0focal_amd64.deb
---
+++ Development/Linux/devdependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Arch Linux development dependencies for FruitControl
+
+sudo yay -S libimobiledevice-git godot
